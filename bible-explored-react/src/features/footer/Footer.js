f8a50e490970f874a7f8724097c88e417dd93503
sergelorenz <sergelorenz.villasica@gmail.com>
2023-08-20 10:36:02 +0800

    Full implmeentation of footer

--- bible-explored-react/src/features/footer/Footer.js
+++ bible-explored-react/src/features/footer/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import apiBibleLogo from '../../res/images/api-logo@2x.png';
+import { ReactComponent as LinkedInIcon } from '../../res/icons/linkedin-square-icon.svg';
+import { ReactComponent as GithubIcon } from '../../res/icons/github-icon.svg';
+
 import './Footer.scss';
 
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className='footer-content'>
+        <hr />
+        <div className='footer-row'>
+          <p>This Bible Web Application makes use of <a href='https://scripture.api.bible/' target='_blank' rel="noreferrer">API.Bible</a> for the retrieval of Bible content</p>
+          <img src={apiBibleLogo} alt='API.Bible Logo' id='api-bible-logo'/>
+        </div>
+        <hr />
+        <div className='footer-row'>
+          <p>Created by Serge Lorenz Villasica as an Open Source Project (2023-2024)</p>
+          <div className='footer-icons'>
+            <a href='https://www.linkedin.com/in/serge-lorenz-villasica/' target='_blank' rel='noreferrer'>
+              <LinkedInIcon />
+            </a>
+            <a href='https://github.com/sergelorenz/' target='_blank' rel='noreferrer'>
+              <GithubIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
