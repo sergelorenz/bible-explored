790f1e7bedae3bc38d55a63484dcd59b7d6df6cc
sergelorenz <sergelorenz.villasica@gmail.com>
2023-09-25 22:24:05 +0800

    initial implementation of light and dark theme using themeService

--- bible-explored-angular/src/app/app.component.ts
+++ bible-explored-angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeService } from './services/theme/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bible-explored-angular';
+export class AppComponent implements OnInit, OnDestroy {
+  theme: string = '';
+  private themeSubscription: Subscription = new Subscription();
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.themeSubscription = this.themeService.getTheme().subscribe(
+      theme => this.theme = theme
+    );
+  }
+
+  ngOnDestroy(): void {
+      if (this.themeSubscription) {
+        this.themeSubscription.unsubscribe();
+      }
+  }
 }
