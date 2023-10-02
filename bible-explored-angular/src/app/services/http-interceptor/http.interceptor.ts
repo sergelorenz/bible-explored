940694d277637407529113b6c89005959013b662
sergelorenz <sergelorenz.villasica@gmail.com>
2023-10-02 22:51:26 +0800

    adding timeout in the interceptor

--- bible-explored-angular/src/app/services/http-interceptor/http.interceptor.ts
+++ bible-explored-angular/src/app/services/http-interceptor/http.interceptor.ts
@@ -6,18 +6,26 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { tap, throwError } from 'rxjs';
 import { Observable, of } from 'rxjs';
+import { timeout, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
 
   private cache = new Map<string, HttpResponse<any>>();
+  private readonly defaultTimeoutMilliseconds = 6000;
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const timeoutRequest = req.clone({
+      setHeaders: {
+        'timeout': this.defaultTimeoutMilliseconds.toString()
+      }
+    })
+
     if (req.method !== 'GET') {
       return next.handle(req);
     }
@@ -28,11 +36,18 @@ export class HttpRequestInterceptor implements HttpInterceptor {
       return of(cachedResponse);
     }
 
-    return next.handle(req).pipe(
+    return next.handle(timeoutRequest).pipe(
+      timeout(this.defaultTimeoutMilliseconds),
       tap((event) => {
         if (event instanceof HttpResponse) {
           this.cache.set(req.urlWithParams, event);
         }
+      }),
+      catchError((error: any) => {
+        if (error.name === 'TimeoutError') {
+          console.error('Request timed out.');
+        }
+        return throwError(() => new Error(error))
       })
     )
   }
