efd98ca9cc894947764514a376c827d6c07cef13
sergelorenz <sergelorenz.villasica@gmail.com>
2023-10-22 14:49:49 +0800

    update on problem-card, successful call to bible api for getPassages()

--- bible-explored-angular/src/app/verse-of-the-day/problem-card/problem-card.component.ts
+++ bible-explored-angular/src/app/verse-of-the-day/problem-card/problem-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { Problem } from 'src/app/shared/types';
+import { BibleService } from 'src/app/services/bible/bible.service';
+import { Problem, GetPassageRequest, PassageContent } from 'src/app/shared/types';
+import { DEFAULT_BIBLE_ID } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-problem-card',
@@ -11,4 +13,34 @@ export class ProblemCardComponent {
     description: '',
     verses: []
   };
+  @Input() bibleVersionId: string = DEFAULT_BIBLE_ID;
+  isOpen = false;
+  isLoading = false;
+  passages?: PassageContent[]
+
+  constructor(private bibleService: BibleService) {}
+
+  toggleOpenPassage() {
+    if (!this.isOpen) {
+      this.getPassages()
+    }
+    this.isOpen = !this.isOpen;
+  }
+
+  getPassages() {
+    let passageRequests: GetPassageRequest[] = this.problemItem.verses.map(verse => ({
+      bibleId: this.bibleVersionId,
+      passage: verse
+    }))
+    this.isLoading = true;
+    this.bibleService.getPassages(passageRequests).subscribe({
+      next: (passages: PassageContent[]) => {
+        this.passages = passages;
+        this.isLoading = false;
+      },
+      error: error => {
+        this.isLoading = true;
+      }
+    })
+  }
 }
