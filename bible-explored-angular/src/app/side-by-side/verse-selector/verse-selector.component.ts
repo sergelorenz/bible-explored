e6381241183dae5bac7cf310ca5edb0279fcd546
sergelorenz <sergelorenz.villasica@gmail.com>
2023-10-27 22:09:27 +0800

    initializing side-by-side component

---
+++ bible-explored-angular/src/app/side-by-side/verse-selector/verse-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verse-selector',
+  templateUrl: './verse-selector.component.html',
+  styleUrls: ['./verse-selector.component.scss']
+})
+export class VerseSelectorComponent {
+
+}
